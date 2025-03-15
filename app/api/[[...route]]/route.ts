fc419d70d1da9345b7c71e0b2f3175af4567fac4
akshithakolan <akshithakolan@gmail.com>
2025-03-14 23:25:00 -0400

    Zod

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,5 +1,7 @@
+import { z } from 'zod'
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+import { zValidator  } from '@hono/zod-validator'
 
 export const runtime = 'edge'
 
@@ -11,15 +13,28 @@ app
             message: 'Hello Next.js!',
         })
     })
-    .get('/hello/:test', (c) => {
-        const test = c.req.param('test')
-
+    .get(
+        '/hello/:test', 
+        (c) => {
         return c.json({
             message: 'Hello World',
-            test: test,
         })
 
     })
+    .post(
+        '/create/:postId',
+        zValidator('json', z.object({
+            name: z.string(),
+            userId: z.number(),
+        })),
+        zValidator('param', z.object({
+            postId: z.number(),
+        })),
+        (c) => {
+            const { name, userId } = c.req.valid('json')
+            const { postId } = c.req.valid('param')
+            return c.json({})
+        })
 
 export const GET = handle(app)
 export const POST = handle(app)
