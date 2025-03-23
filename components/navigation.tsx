185c45f91f818856a78feafa51f689a4ebaeb96f
akshithakolan <akshithakolan@gmail.com>
2025-03-22 21:37:59 -0400

    Navbar

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -27,7 +27,11 @@ export const Navigation = () => {
     return (
         <nav className='hidden lg:flex items-center gap-x-2 overflow-x-auto'>
             {routes.map((route) => (
-                <p> {route.label} </p>
+                <NavButton
+                key={route.href}
+                href={route.href}
+                label={route.label}
+                />
             ))}
         </nav>
     )
