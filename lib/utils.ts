4bbd4461f76ea7ca18a77fb4606b0901c378e6b6
akshithakolan <akshithakolan@gmail.com>
2025-06-09 18:43:38 -0400

    Enhanced Nav-Bar

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,8 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+
+// This function can be used to dynamically change tailwind classes
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
